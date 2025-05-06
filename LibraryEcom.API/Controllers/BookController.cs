c9d30c3731e98b2caff3846c9a9e8e080f4ce046
paleswan1 <pallu.sth1@gmail.com>
2025-05-06 11:10:21 +0545

    Upload and Delete Image

--- LibraryEcom.API/Controllers/BookController.cs
+++ LibraryEcom.API/Controllers/BookController.cs
@@ -1,93 +1,120 @@
-using System.Net;
-using LibraryCom.Controllers.Base;
-using LibraryEcom.Application.Common.Response;
-using LibraryEcom.Application.DTOs.Book;
-using LibraryEcom.Application.Interfaces.Services;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+    using System.Net;
+    using LibraryCom.Controllers.Base;
+    using LibraryEcom.Application.Common.Response;
+    using LibraryEcom.Application.DTOs.Book;
+    using LibraryEcom.Application.Interfaces.Services;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
 
-namespace LibraryCom.Controllers;
+    namespace LibraryCom.Controllers;
 
-[Route("api/book")]
-[ApiController]
-[Authorize]
-public class BookController(IBookService bookService) : BaseController<BookController>
-{
-    [HttpGet]
-    public IActionResult Get(int pageNumber, int pageSize, string? search = null)
+    [Route("api/book")]
+    [ApiController]
+    [Authorize]
+    public class BookController(IBookService bookService) : BaseController<BookController>
     {
-        var books = bookService.GetAll(pageNumber, pageSize, out var rowCount, search);
-
-        return Ok(new CollectionDto<BookDto>(books, rowCount, pageNumber, pageSize)
+        [HttpGet]
+        public IActionResult Get(int pageNumber, int pageSize, string? search = null)
         {
-            StatusCode = (int)HttpStatusCode.OK,
-            Message = "Books retrieved successfully.",
-            Result = books
-        });
-    }
+            var books = bookService.GetAll(pageNumber, pageSize, out var rowCount, search);
 
-    [HttpGet("list")]
-    public IActionResult GetList(string? search = null)
-    {
-        var books = bookService.GetAll(search);
+            return Ok(new CollectionDto<BookDto>(books, rowCount, pageNumber, pageSize)
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Message = "Books retrieved successfully.",
+                Result = books
+            });
+        }
 
-        return Ok(new ResponseDto<List<BookDto>>
+        [HttpGet("list")]
+        public IActionResult GetList(string? search = null)
         {
-            StatusCode = (int)HttpStatusCode.OK,
-            Message = "Book list retrieved successfully.",
-            Result = books
-        });
-    }
+            var books = bookService.GetAll(search);
 
-    [HttpGet("{id:guid}")]
-    public IActionResult GetById(Guid id)
-    {
-        var book = bookService.GetById(id);
+            return Ok(new ResponseDto<List<BookDto>>
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Message = "Book list retrieved successfully.",
+                Result = books
+            });
+        }
 
-        return Ok(new ResponseDto<BookDto>
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById(Guid id)
         {
-            StatusCode = (int)HttpStatusCode.OK,
-            Message = "Book retrieved successfully.",
-            Result = book
-        });
-    }
+            var book = bookService.GetById(id);
 
-    [HttpPost]
-    public IActionResult Create([FromForm] CreateBookDto bookDto)
-    {
-        bookService.Create(bookDto);
+            return Ok(new ResponseDto<BookDto>
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Message = "Book retrieved successfully.",
+                Result = book
+            });
+        }
 
-        return Ok(new ResponseDto<bool>
+        [HttpPost]
+        public IActionResult Create([FromForm] CreateBookDto bookDto)
         {
-            StatusCode = (int)HttpStatusCode.OK,
-            Message = "Book created successfully.",
-            Result = true
-        });
-    }
+            bookService.Create(bookDto);
 
-    [HttpPut("{id:guid}")]
-    public IActionResult Update(Guid id, [FromBody] UpdateBookDto bookDto)
-    {
-        bookService.Update(id, bookDto);
+            return Ok(new ResponseDto<bool>
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Message = "Book created successfully.",
+                Result = true
+            });
+        }
 
-        return Ok(new ResponseDto<bool>
+        [HttpPut("{id:guid}")]
+        public IActionResult Update(Guid id, [FromBody] UpdateBookDto bookDto)
         {
-            StatusCode = (int)HttpStatusCode.OK,
-            Message = "Book updated successfully.",
-            Result = true
-        });
-    }
+            bookService.Update(id, bookDto);
 
-    [HttpDelete("{id:guid}")]
-    public IActionResult Delete(Guid id)
-    {
-        bookService.Delete(id);
+            return Ok(new ResponseDto<bool>
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Message = "Book updated successfully.",
+                Result = true
+            });
+        }
 
-        return Ok(new ResponseDto<bool>
+        [HttpDelete("{id:guid}")]
+        public IActionResult Delete(Guid id)
         {
-            StatusCode = (int)HttpStatusCode.OK,
-            Message = "Book deleted successfully.",
-            Result = true
-        });
+            bookService.Delete(id);
+
+            return Ok(new ResponseDto<bool>
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Message = "Book deleted successfully.",
+                Result = true
+            });
+        }
+        
+        [HttpPost("upload-image")]
+        public IActionResult UploadImage([FromForm] IFormFile file)
+        {
+            var fileName = bookService.UploadImage(file);
+
+            return Ok(new ResponseDto<string>
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Message = "Image uploaded successfully.",
+                Result = fileName
+            });
+        }
+
+        [HttpDelete("delete-image")]
+        public IActionResult DeleteImage([FromQuery] string fileName)
+        {
+            bookService.DeleteImage(fileName);
+
+            return Ok(new ResponseDto<bool>
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Message = "Image deleted successfully.",
+                Result = true
+            });
+        }
+
     }
-}

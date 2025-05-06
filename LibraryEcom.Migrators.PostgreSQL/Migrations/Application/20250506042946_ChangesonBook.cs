d1a0a1dbd6cfc1abae3b68763cffe1b49dc17fba
paleswan1 <pallu.sth1@gmail.com>
2025-05-06 10:16:32 +0545

    Unversioned File

---
+++ LibraryEcom.Migrators.PostgreSQL/Migrations/Application/20250506042946_ChangesonBook.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryEcom.Migrators.PostgreSQL.Migrations.Application
+{
+    /// <inheritdoc />
+    public partial class ChangesonBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
